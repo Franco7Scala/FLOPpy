664cf8b7be5036d7e5c6c3f2d57f1ec14a5341f6
francescomandarino15 <francescomandarino81@gmail.com>
2025-11-25 20:55:55 +0100

    Create base_logger.py

---
+++ FLOP_Tracker/ft_logging/base_logger.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class BaseLogger(ABC):
+    def __init__(self, log_per_batch: bool, log_per_epoch: bool):
+        self.log_per_batch = log_per_batch
+        self.log_per_epoch = log_per_epoch
+
+    @abstractmethod
+    def log_batch(self, step: int, flop: int, cumulative_flop: int, epoch: int | None = None):
+        ...
+
+    @abstractmethod
+    def log_epoch(self, epoch: int, flop: int, cumulative_flop: int):
+        ...
+
+    @abstractmethod
+    def close(self):
+        ...
