9f514450eae7934ddddac2bfc2cd23c50d460bb5
francescomandarino15 <francescomandarino81@gmail.com>
2026-03-19 21:36:46 +0100

    Update base_logger.py

--- src/floppy/logging/base_logger.py
+++ src/floppy/logging/base_logger.py
@@ -4,15 +4,19 @@ from abc import ABC, abstractmethod
 
 class BaseLogger(ABC):
     """
-    minimal Logger:
-    - only receive the final summary
-    - optionally export / send
+    Minimal logger interface.
     """
+
+    def log_batch(self, summary: dict):
+        return summary
+
+    def log_epoch(self, summary: dict):
+        return summary
 
     @abstractmethod
     def log_summary(self, summary: dict):
         """
-        Receives the final FLOP summary and return it
+        Receives the final FLOP summary and may return it.
         """
         ...
 
