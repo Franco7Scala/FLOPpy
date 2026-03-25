548aa3e9ad9e7604b49547815b138e1692ddf3b4
francescomandarino15 <francescomandarino81@gmail.com>
2026-03-25 14:35:46 +0100

    Update hf_transformer_example.py

--- examples/hf_transformer_example.py
+++ examples/hf_transformer_example.py
@@ -33,7 +33,6 @@ with torch.no_grad():
     outputs = model(**inputs)
 
 report = tracker.report()
-print_report_metrics(report)
 
 # ============================================================
 # MODE 2: context-manager style
@@ -58,4 +57,3 @@ with FLOPpyTracker(run_name="hf_transformer_test_with") as tracker:
     tracker.stop()
 
 report = tracker.report()
-print_report_metrics(report)
