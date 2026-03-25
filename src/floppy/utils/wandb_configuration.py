3794317dfb2e85c076ffbe5a384738f905e501a3
Francesco Scala <franco7scala@gmail.com>
2026-03-25 11:03:15 +0100

    Update wandb_configuration.py

--- src/floppy/utils/wandb_configuration.py
+++ src/floppy/utils/wandb_configuration.py
@@ -1,13 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class WandbConfiguration:
     """
         Configuration settings for Weights & Biases (WandB) integration.
         Used to synchronize real-time tracking of computational workloads.
     """
 
-    def __init__(self, project_name: str, group_name: str, reporter_key: str):
-        #: The name of the WandB project where the runs will be logged.
-        self.project_name = project_name
-        #: The group name to organize multiple runs together in the W&B dashboard.
-        self.group_name = group_name
-        #: The API key used for authenticating with the WandB server.
-        self.reporter_key = reporter_key
+    #: The name of the WandB project where the runs will be logged.
+    project_name: str
+    #: The group name to organize multiple runs together in the W&B dashboard.
+    group_name: str
+    #: The API key used for authenticating with the WandB server.
+    reporter_key: str
